d0a763938523ded0e2dfadbfdf3add714dea33e3
Nikita Schastnyy <smynay@yandex.ru>
2021-04-26 00:47:13 +0300

    Add data to beer card

--- src/app/components/card/card.jsx
+++ src/app/components/card/card.jsx
@@ -1,11 +1,11 @@
 import "./card.scss";
 
 export function Card({ props }) {
-  const { id, name, tagline, description, image_url } = props;
+  const { name, tagline, description, image_url, abv, first_brewed } = props;
 
   return (
     <div className="card shadow-lg d-flex flex-column h-100">
-      <img src={image_url} className="card__img card-img-top" alt="Beer image" />
+      <img src={image_url} className="card__img card-img-top" alt="" />
       <div className="card-body flex-grow-1 flex-shrink-1">
         <h5 className="card-title m-0">{name}</h5>
         <p className="card-text text-secondary font-weight-light">{tagline}</p>
@@ -14,13 +14,14 @@ export function Card({ props }) {
           {description.length > 220 ? " ..." : ""}
         </p>
       </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Alcohol by volume: {abv}%</li>
+        <li className="list-group-item">First brewed: {first_brewed}</li>
+      </ul>
       <div className="card-body flex-grow-0 flex-shrink-0">
-        <a href="#" className="card-link text-warning">
-          Watch the beer
-        </a>
-        <a href="#" className="card-link text-warning">
-          Beer-chief page
-        </a>
+        <button className="card-link btn btn-warning" disabled>
+          Watch the beer (soon)
+        </button>
       </div>
     </div>
   );
